0bb4335c3d8e641db4cb4888d17d72bca5ac9dde
santiagokasses <101513911+santiagokasses@users.noreply.github.com>
2023-05-03 10:13:03 -0300

    si

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import express from "express";
 import PersonajeService from "./src/services/Personajes-services.js"
 import PeliculasService from "./src/services/Peliculas-services.js";
 
+import index from "./src/pages/index.js";
+import login from "./src/pages/login.js";
+
 var app = express()
 
 // Creamos una ruta para el camino por defecto
-app.get('/', function(req, res) {
-    res.send('Soy la Home Page!');
-})
+app.get('/', (req, res) => res.send(index))
+
 
 
 app.get('/about', function(req, res) {
@@ -20,35 +22,35 @@ app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
 })
 
+
 var deleteid = 6
 var id = 1
 //deleteById(deleteid)
 //getAll()
-getById(id)
+//getById(id)
 async function deleteById(deleteid){
     let svc = new PersonajeService();
     let data;
 
     data = await svc.deleteById(deleteid)
-    console.log(data);
+    console.log(data)
 
 }
 async function getAll(){
     let svc = new PersonajeService();
     let data;
-
+    
     data = await svc.getAll()
-    console.log(data);
+    console.log(data)
 }
 async function getById(id){
     let svc = new PersonajeService();
     let data;
 
     data = await svc.getById(id)
-    console.log(data);
+    console.log(data)
 
 }
-
 
 // Ejecutamos la app escribiendo en la consola: node index.js
 // Vamos al explorador y escribimos este link: http://localhost:3000
---
+++ src/pages/login.js
@@ -0,0 +1,7 @@
+const login = `
+    <a href="/">Volver al index</a>
+    <h1>/auth/login</h1>
+`
+
+
+export default login
