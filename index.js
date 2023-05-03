4f00a567910e110d66aeb056c97ac5e500093647
santiagokasses <101513911+santiagokasses@users.noreply.github.com>
2023-05-03 09:39:00 -0300

    express

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+var express = require('express')
 import PersonajeService from "./src/services/Personajes-services.js"
 import PeliculasService from "./src/services/Peliculas-services.js";
+
+var app = express()
+
+// Creamos una ruta para el camino por defecto
+app.get('/', function(req, res) {
+    res.send('Soy la Home Page!');
+})
+
+
+app.get('/about', function(req, res) {
+    res.send('Soy la pagina del about!');
+})
+
+
+// Iniciamos el servidor
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
+})
+
+// Ejecutamos la app escribiendo en la consola: node index.js
+
+// Vamos al explorador y escribimos este link: http://localhost:3000
+
 
 var deleteid = 6
 var id = 1
