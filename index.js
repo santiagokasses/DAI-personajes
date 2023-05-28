1133bbd9de022ec334d6f62dfdd4200d28e0cd4a
CamilaAylenLopez <camilalopezort@gmail.com>
2023-05-28 02:31:37 -0300

    descomentando funciones y llamados a funciones

--- index.js
+++ index.js
@@ -25,95 +25,98 @@ personaje = {Nombre : "Hanz landa", Imagen: "https://imagenes.20minutos.es/files
 var Kitty = new Personaje()
 Kitty = {id: 10, imagen: 'https://static.wikia.nocookie.net/doblaje/images/b/b5/Kitianime.jpg/revision/latest/thumbnail/width/360/height/450?cb=20171001023505&path-prefix=es', nombre:'Kitty', edad:10,peso:15,historia:'es un gato',peliserie:'Hello Kitty' }
 
+var KittyDOS = new Personaje()
+KittyDOS = {id: 10, imagen: 'https://static.wikia.nocookie.net/doblaje/images/b/b5/Kitianime.jpg/revision/latest/thumbnail/width/360/height/450?cb=20171001023505&path-prefix=es', nombre:'Kitty EL GATO', edad:10,peso:15,historia:'es un gato FELIZ',peliserie:'Hello Kitty :)' }
+
 ////////////////Personajes//////////////////
-async function deleteByIdPersonaje(id){
-    let svc = new PersonajeService()
-    let data
-    data = await svc.deleteById(id)
-    console.log(data)
-}
 async function getAllPersonaje(){
     let svc = new PersonajeService()
     let data
     data = await svc.getAll()
     console.log(data)
 }
+//getAllPersonaje()
+
 async function getByIdPersonaje(id){
     let svc = new PersonajeService()
     let data
     data = await svc.getById(id)
     console.log(data)
 }
+getByIdPersonaje(5)
 
-
-deleteByIdPersonaje(6)
-//getAllPersonaje()
-//getByIdPersonaje(10)
-
-////////NO FUNCIONAN/////////
-/*
 async function AddPersonaje(PersonajeNuevo){
     let svc = new PersonajeService()
     let data
     data = await svc.insert(PersonajeNuevo)
     console.log(data)
 }
+AddPersonaje(Kitty)
 
-async function UpdatePersonaje(id){
+async function UpdatePersonaje(personajeModificado){
     let svc = new PersonajeService()
     let data
     data = await svc.update(personaje)
     console.log(data)
 }
-*/
-//UpdatePersonaje(id)
-//AddPersonaje(Kitty)
-////////NO FUNCIONAN/////////
+UpdatePersonaje(KittyDOS)
 
-////////////////PELICULAS//////////////////
-async function deleteByIdPelicula(id){
-    let svc = new PeliculasService()
+async function deleteByIdPersonaje(id){
+    let svc = new PersonajeService()
     let data
     data = await svc.deleteById(id)
     console.log(data)
 }
+deleteByIdPersonaje(6)
+
+////////////////PELICULAS//////////////////
+let HelloKitty = new Peliculas
+HelloKitty = {id: '10', imagen: 'https://static.wikia.nocookie.net/doblaje/images/b/b5/Kitianime.jpg/revision/latest/thumbnail/width/360/height/450?cb=20171001023505&path-prefix=es', titulo: 'Hello Kitty', fechaCreacion: '20/02/2009', calificacion:'10', PersonajesAsociados: '10'}
+
+let HelloKittyDOS = new Peliculas
+HelloKittyDOS = {id: '10', imagen: 'https://static.wikia.nocookie.net/doblaje/images/b/b5/Kitianime.jpg/revision/latest/thumbnail/width/360/height/450?cb=20171001023505&path-prefix=es', titulo: 'Hello Kitty DOS', fechaCreacion: '20/02/2010', calificacion:'9', PersonajesAsociados: '10'}
+
 async function getAllPelicula(){
     let svc = new PeliculasService()
     let data
     data = await svc.getAll()
     console.log(data)
 }
+//getAllPelicula()
+
 async function getByIdPelicula(id){
     let svc = new PeliculasService()
     let data
     data = await svc.getById(id)
     console.log(data)
 }
+getByIdPelicula(5)
 
-let HelloKitty = new Peliculas
-HelloKitty = {id: '10', imagen: 'https://static.wikia.nocookie.net/doblaje/images/b/b5/Kitianime.jpg/revision/latest/thumbnail/width/360/height/450?cb=20171001023505&path-prefix=es', titulo: 'Hello Kitty', fechaCreacion: '20/02/2009', calificacion:'10', PersonajesAsociados: '10'}
-
-//getAllPelicula()
-//getByIdPelicula(10)
-
-////////NO FUNCIONAN/////////
-/*
 async function AddPelicula(PeliculaNueva){
     let svc = new PeliculasService()
     let data
     data = await svc.insert(PeliculaNueva)
     console.log(data)
 }
-async function UpdatePelicula(id){
+AddPelicula(HelloKitty)
+
+async function UpdatePelicula(peliculaModificada){
     let svc = new PeliculasService()
     let data
     data = await svc.update(personaje)
     console.log(data)
 }
-*/
-//UpdatePelicula(id)
-//AddPelicula(Kitty)
-////////NO FUNCIONAN/////////
+UpdatePelicula(HelloKittyDOS)
+
+async function deleteByIdPelicula(id){
+    let svc = new PeliculasService()
+    let data
+    data = await svc.deleteById(id)
+    console.log(data)
+}
+deleteByIdPelicula(6)
+
+
 
 // Ejecutamos la app escribiendo en la consola: node index.js
 // Vamos al explorador y escribimos este link: http://localhost:3000
